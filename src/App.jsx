ce33b8f025beae8da26c7db75a83df11641a73c1
vanshika704 <sharmavanshi704@gmail.com>
2024-06-09 21:38:44 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import Gallery from "./pages/backgroundCarousel";
-// import NavBar from "./pages/navbar";
+
 import Textgreen from "./pages/textgreen";
 
 function App() {
   return (
     <div>
-      {/* <NavBar/> */}
+  
       <Gallery />
       <Textgreen/>
     </div>
--- src/pages/textgreen.jsx
+++ src/pages/textgreen.jsx
@@ -1,4 +1,10 @@
+import { FaEye, } from "react-icons/fa";
 function Textgreen (){
-    return (<><div className="textgreen">hello</div></>)
+    return (<><div className="textgreen"><div className="text1">What is Forest Hill ?</div>
+    <div className="text2">Forest Hill, a unique blend of hotel and cottage nestled in the scenic outskirts of Shimla amid the breath-taking hills of<br></br> Ghanahatti. Immerse yourself in the unparalleled beauty of nature, offering a serene sanctuary to unwind and escape the<br></br> hustle of city life. Discover a tranquil haven that invites you to relax, rejuvenate, and embrace the soothing ambiance of this<br></br> picturesque retreat.</div>
+    <div className="text3">Forest Hill Retreat</div>
+    <div className="text2">Escape to Forest Hill, a serene hotel cum cottage just 12 km from Shimla. Nestled in the hills, our haven offers a tranquil retreat from city life. Immerse yourself in  Nature&apos;s beauty and savor local cuisine for a truly rejuvenating experience. Forest Hill Hotel: Where serenity meets mountain hospitality.</div>
+    <button className="button3"  ><FaEye /> View</button> </div>
+    </>)
 }
 export default Textgreen;
