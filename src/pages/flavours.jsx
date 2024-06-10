e9411fe4edf6a198331bece67d7a7092f598a2b8
vanshika704 <sharmavanshi704@gmail.com>
2024-06-10 13:28:49 +0530

    .

--- src/pages/flavours.jsx
+++ src/pages/flavours.jsx
@@ -1,4 +1,20 @@
-function Flavours (){
-    return (<><div className="flavours"><div className="text4">Flavours at Forest Hill</div></div></>)
+import { FaSmog } from "react-icons/fa";
+
+function Flavours() {
+  return (
+    <>
+      <div className="flavours">
+        <div className="text4">Flavours at Forest Hill</div>
+        <div className="listflavours">
+          <ul className="flavourlist">
+            <li>
+              <FaSmog color=" rgb(70, 82, 38)" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
+
 export default Flavours;
