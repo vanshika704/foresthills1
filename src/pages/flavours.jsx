c223fff920a82c5aa70aeb1a7dc545975a724051
vanshika704 <sharmavanshi704@gmail.com>
2024-06-13 21:27:00 +0530

    new

--- src/pages/flavours.jsx
+++ src/pages/flavours.jsx
@@ -1,5 +1,7 @@
-import { FaSmog } from "react-icons/fa";
-
+import { GrGallery } from "react-icons/gr";
+import { LuChefHat } from "react-icons/lu";
+import { FaBellConcierge } from "react-icons/fa6";
+import { LuMenu } from "react-icons/lu";
 function Flavours() {
   return (
     <>
@@ -8,9 +10,21 @@ function Flavours() {
         <div className="listflavours">
           <ul className="flavourlist">
             <li>
-              <FaSmog color=" rgb(70, 82, 38)" />Crafting Culinary Magic, Helmed by Our Maestro Chef.
+              <LuChefHat color="rgb(70, 82, 38)" size={25} className="icon" />
+              Crafting Culinary Magic, Helmed by Our Maestro Chef.
             </li>
-            
+            <li>
+              <FaBellConcierge color="rgb(70, 82, 38)" size={25} className="icon" />
+              Global Tastes, Local Flair: A Fusion Feast of Indian, Himachali, and Chinese Delights.
+            </li>
+            <li>
+              <LuMenu color="rgb(70, 82, 38)" size={25} className="icon" />
+              Savor Simplicity: Order with Ease from a Menu Tailored to Your Tastes.
+            </li>
+            <li>
+              <GrGallery color="rgb(70, 82, 38)" size={25} className="icon" />
+              Nature&#39;s Canvas Unveiled: Panoramic Views Through Our Enchanting Windows.
+            </li>
           </ul>
         </div>
       </div>
