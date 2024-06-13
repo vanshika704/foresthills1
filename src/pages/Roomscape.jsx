1121527fcb9f4552523cabb12a9505225d75bf8a
vanshika704 <sharmavanshi704@gmail.com>
2024-06-13 22:22:38 +0530

    .

--- src/pages/Roomscape.jsx
+++ src/pages/Roomscape.jsx
@@ -1,4 +1,5 @@
 function Roomscape(){
-    return(<><div className="roomscape"><div className="Roomscape">&quot; Roomscape &quot;</div></div></>)
+    return(<><div className="roomscape"><div className="Roomscape">&quot; Roomscape &quot;</div>
+    <div className="line2">&quot;Five Rooms, Five Worlds: Tailored Experiences Awaits Your Stay.&quot;</div></div></>)
 }
 export default Roomscape; 
