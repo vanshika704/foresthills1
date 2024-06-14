d66eb5b14db93158f5c97901f5f0c493759220d3
vanshika704 <sharmavanshi704@gmail.com>
2024-06-14 21:49:21 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import Gallery from "./pages/backgroundCarousel";
 import Flavours from "./pages/flavours";
+import Nearest from "./pages/nearestlocalities";
 import Roomscape from "./pages/Roomscape";
 
 import Textgreen from "./pages/textgreen";
@@ -13,6 +14,7 @@ function App() {
       <Textgreen/>
       <Flavours/>
       <Roomscape/>
+      <Nearest/>
     </div>
   );
 }
